4727cf2e535ccb65b09d35afbc45c9ec849c84da
Kevin4545 <78331792+Kevin4545@users.noreply.github.com>
2021-02-02 10:19:41 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,8 @@
+score1=int(input("請輸入數學成績"))
+socre2=int(input("請輸入英文成績")
+if not((score1>=0 and score<=100) and ((score2>=0 and score<=100):
+    print("please put 0 to 100 midlle score)
+elif((score>=90 and score2>=90 or score1 ==100or score2==100):
+    print("you got scholarship")
+else:
+    print("more")
